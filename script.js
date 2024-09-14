bd392a21f9e91d7c06b06d6b53b382b92a5b3e5f
centevo <144074037+centevo@users.noreply.github.com>
2024-09-13 20:50:14 -0600

    k

--- script.js
+++ script.js
@@ -3,51 +3,63 @@ const ctx = c.getContext('2d')
 let cw = c.width = innerWidth
 let ch = c.height = innerHeight
 
-const ticks = Array(200)
-const dur = 4
+const ticks = 150
+const ring1 = []
+const ring2 = []
+const dur = 12
 
-for (let i=0; i<ticks.length; i++){
-  ticks[i] = {
+for (let i=0; i<ticks; i++){
+  const angle = i / ticks * Math.PI * 2
+  const radius = 250
+  ring1[i] = {
     x1:0, x2:0, y1:0, y2:0,
-    lineWidth: 60,
-    angle: i / ticks.length * Math.PI * 2,
-    r: 390,
-    h: 145+ gsap.utils.wrapYoyo(0, 80, i/ticks.length*160)
+    lineWidth: 6,
+    a: angle,
+    r: radius,
+    h: 180+ gsap.utils.wrapYoyo(0, 40, i/ticks*160)
+  }
+  ring2[i] = {
+    x1:0, x2:0, y1:0, y2:0,
+    lineWidth: 2,
+    a: angle,
+    r: radius/2,
+    h: 180+ gsap.utils.wrapYoyo(0, 40, i/ticks*160)
   }
 }
 
 const tl = gsap.timeline({onUpdate:update})
-.fromTo(ticks, {
-  x1:(i,t)=> Math.cos(t.angle)*t.r,
-  y1:(i,t)=> Math.sin(t.angle)*t.r,
-  x2:(i,t)=> Math.cos(t.angle)*t.r*-1.1,
-  y2:(i,t)=> 0,//Math.sin(t.angle)*t.r*-1.1,
+.fromTo([ring1,ring2], {
+  x1:(i,t)=> Math.cos(t.a)*t.r*-2,
+  y1:(i,t)=> Math.sin(t.a)*t.r*-2,
+  x2:(i,t)=> Math.cos(t.a)*t.r*15,
+  y2:(i,t)=> Math.sin(t.a)*t.r*8,
 },{
-  x1:(i,t)=> Math.cos(t.angle)*t.r*.7,
-  y1:(i,t)=> Math.sin(t.angle)*t.r*.7,
-  x2:(i,t)=> Math.cos(t.angle)*t.r*.25,
-  y2:(i,t)=> Math.sin(t.angle)*t.r*.25,
-  duration:dur,
-  ease:'power3.inOut',
-  stagger:{amount:dur*10, from:0, repeat:-1, yoyo:true}
+  x1:(i,t)=> Math.cos(t.a)*t.r*.3,
+  y1:(i,t)=> Math.sin(t.a)*t.r*.3,
+  x2:(i,t)=> Math.cos(t.a)*t.r*.12,
+  y2:(i,t)=> Math.sin(t.a)*t.r*.12,
+  duration:dur/2,
+  ease:'back',
+  repeat:-1,
+  yoyo:true
 }, 0)
-.to(ticks, {
+.to(ring1, {
   lineWidth: 1,
-  duration:dur*.5,
-  h:'+=90',
+  h:'+=120',
+  duration:dur*.25,
   // ease:'circ.inOut',
-  // ease:'power4',
-  // yoyoEase:'power2.in',
-  stagger:{amount:dur*2, from:'end', repeat:-1, yoyo:true}
+  ease:'power4',
+  yoyoEase:'power2.in',
+  stagger:{amount:dur, from:0, repeat:-1, yoyo:true}
 }, 0)
-.play(999)
+.play(dur*1.5)
 
 
 function drawPath(t){
   ctx.strokeStyle = 'hsl('+t.h+',100%,50%)'
   ctx.lineCap = "round"
   ctx.lineWidth = t.lineWidth
-  ctx.setLineDash([t.lineWidth/2, 40])
+  ctx.setLineDash([t.lineWidth*2, 30])
   ctx.beginPath()
   ctx.moveTo(t.x1+cw/2, t.y1+ch/2);
   ctx.lineTo(t.x2+cw/2, t.y2+ch/2);
@@ -56,8 +68,8 @@ function drawPath(t){
 
 function update(){
   ctx.clearRect(0,0,cw,ch)
-  ctx.globalCompositeOperation = "lighter"
-  ticks.forEach(drawPath)
+  ring1.forEach(drawPath)
+  ring2.forEach(drawPath)
 }
 
 window.onresize = ()=>{
@@ -68,8 +80,8 @@ window.onresize = ()=>{
 
 window.onpointerup =()=>{ //toggle play/pause on click
    gsap.to(tl, {
-     duration:1.2,
-     ease:'sine.inOut',
+     duration:1,
+     ease:'power3',
      timeScale:( tl.isActive() ? 0 : 1 )
    })
 }
