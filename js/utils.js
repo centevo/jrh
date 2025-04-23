bbf793db5cb619e0644feb466833b47af151b74b
centevo <144074037+centevo@users.noreply.github.com>
2025-04-22 18:38:17 -0600

    ds

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,18 @@
-/**
- * Preloads images specified by the CSS selector.
- * @function
- * @param {string} [selector='img'] - CSS selector for target images.
- * @returns {Promise} - Resolves when all specified images are loaded.
- */
-const preloadImages = (selector = 'img') => {
-  return new Promise((resolve) => {
-      // The imagesLoaded library is used to ensure all images (including backgrounds) are fully loaded.
-      imagesLoaded(document.querySelectorAll(selector), {background: true}, resolve);
-  });
+// Preload images
+const preloadFonts = id => {
+    return new Promise((resolve) => {
+        WebFont.load({
+            typekit: {
+                id: id
+            },
+            active: resolve
+        });
+    });
 };
 
-// Exporting utility functions for use in other modules.
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 export {
-  preloadImages
-};
+    preloadFonts,
+    randomNumber
+}
